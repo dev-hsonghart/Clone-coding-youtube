8923fcd9e4cf3c9b54d7bc4f16082bec695e5abc
hsh-newbi <hsonghart0426@gmail.com>
2021-03-25 16:50:42 +0900

    상단 gnb css 작성

--- js/controlNAV.js
+++ js/controlNAV.js
@@ -8,21 +8,24 @@ function mediaQueryNav(){
   // screen < 450px 일 때 gnbCenter에 display에 none 추가 , 우측 mic와 search에 display에 빈값을 준다.
   // 우측 mic와 search는 display 기본값이 none이다.
   const navWidth450 = window.matchMedia("(max-width: 450px)"),
-    navWidth360 = window.matchMedia("(max-width: 360px)");
-    if(navWidth360.matches === true){
+    navWidth360 = window.matchMedia("(max-width: 400px)");
+
+    if(navWidth360.matches === true){ // 최소값 400px미만일 때
       rightNoti.style.display = "none";
       rightMic.style.display = "none";
-    } else if(navWidth450.matches === true){
-    //450px 미만
+      gnbRight.style.minWidth= "160px";
+    } else if(navWidth450.matches === true){ // 360px ~ 450px
     gnbCenter.style.display = "none";
     rightMic.style.display = "";
     rightSearch.style.display = "";
     rightNoti.style.display = "";
-  } else{
+    gnbRight.style.minWidth= "240px";
+  } else{ // 450px ~
     gnbCenter.style.display = "";
     rightMic.style.display = "none";
     rightSearch.style.display = "none";
     rightNoti.style.display = "";
+    gnbRight.style.minWidth= "160px";
   }
 }
 
@@ -33,10 +36,12 @@ function init(){
     gnbCenter.style.display = "none";
     rightMic.style.display = "";
     rightSearch.style.display = "";
+    rightNoti.style.display = "";
   } else{
     gnbCenter.style.display = "";
     rightMic.style.display = "none";
     rightSearch.style.display = "none";
+    rightNoti.style.display = "";
   }
   window.addEventListener("resize", mediaQueryNav);
 }
