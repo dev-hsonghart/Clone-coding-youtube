cecea13e58e97db0e70aa06bb4c06d6766d038df
hsh-newbi <hsonghart0426@gmail.com>
2021-03-27 16:26:31 +0900

    숨겨진 메뉴 더 보기 구현
    array 안에 array 안 요소를 불러오고 체크하는 방법 연구 필요

---
+++ js/toggleMore.js
@@ -0,0 +1,62 @@
+// 더보기를 누르면 감춰진 메뉴가 보이고 한번 더 누르면 보여진 메뉴가 숨겨진다.
+const hiddenNav = document.querySelector(".side-nav__list--hidden"),
+  hiddenMore = hiddenNav.querySelector(".normalMore"),
+  hiddenSubMore = hiddenNav.querySelector(".subMore"),
+  hiddenSubscribe = hiddenNav.querySelector(".subscribe"),
+  defaultNav = document.querySelector(".side-nav__list"),
+  defaultMore = defaultNav.querySelector(".normalMore"),
+  subscribeList = defaultNav.querySelector(".subscribe"),
+  subscribeMore = defaultNav.querySelector(".subMore");
+
+function printCurrentSubscribe(){
+  // 구독 카테고리에 숨겨진 구독채널 갯수 표시
+  const subHidden = subscribeList.querySelectorAll(".hidden"),
+  title = subscribeMore.querySelector(".side-menu__title"),
+  subHidden2 = hiddenSubscribe.querySelectorAll(".hidden"),
+  title2 = hiddenSubMore.querySelector(".side-menu__title");
+  
+  title.innerText = `${subHidden.length}개 더보기`;
+  title2.innerText = `${subHidden2.length}개 더보기`;
+}
+
+function toggleMenu(e){
+  const ul = e.target.parentNode.parentNode.parentNode,
+    li = e.target.parentNode.parentNode,
+    hidden = ul.querySelectorAll(".hidden"),
+    icon = li.querySelector("i"),
+    title = li.querySelector(".side-menu__title");
+  
+  if(ul.className === "side-nav__ul second-menu"){
+    for(let i = 0; i < hidden.length; i++){
+      hidden[i].classList.toggle("none");
+    }
+    if(icon.className === "fas fa-chevron-down"){
+      icon.className = "fas fa-chevron-up"
+      title.innerText = "간략히 보기"
+    } else{
+      icon.className = "fas fa-chevron-down"
+      title.innerText = "더보기"
+    }
+  } else if(ul.className === "side-nav__ul subscribe"){
+    for(let i = 0; i < hidden.length; i++){
+      hidden[i].classList.toggle("none");
+    }
+    if(icon.className === "fas fa-chevron-down"){
+      icon.className = "fas fa-chevron-up"
+      title.innerText = "간략히 보기"
+    } else{
+      icon.className = "fas fa-chevron-down"
+      title.innerText = `${hidden.length}개 더보기`
+    }
+  }
+}
+
+function init(){
+  printCurrentSubscribe();
+  defaultMore.addEventListener("click", toggleMenu);
+  hiddenMore.addEventListener("click", toggleMenu);
+  subscribeMore.addEventListener("click", toggleMenu);
+  hiddenSubMore.addEventListener("click", toggleMenu);
+}
+
+init();
